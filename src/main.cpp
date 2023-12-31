9ace0244a86ca1edde748fabd28d00776ec24638
yamaha-8root3 <128972529+Yamaha-8root3@users.noreply.github.com>
2023-12-31 16:26:48 +0900

    姿勢データのみ出力

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,10 @@
 #include <M5Stack.h>
-#define RAD_TO_DEG 57.295779513082320876798154814105
+// #define RAD_TO_DEG 57.295779513082320876798154814105
 
-float accX, accY, accZ;     // 加速度
-float gyroX, gyroY, gyroZ;  // 角速度
-float pitch, roll, yaw;     // 姿勢
-float pitch2, roll2;
+float pitch, roll, yaw;  // 姿勢
+// float pitch2, roll2;
 
-float AccOffset[3] = {0, 0, 0};   // 加速度オフセット[x,y,z]
-float GyroOffset[3] = {0, 0, 0};  // 角速度オフセット[x,y,z]
-float PRYOffset[3] = {0, 0, 0};   // 姿勢オフセット[pitch,roll,yaw]
+float PRYOffset[3] = {0, 0, 0};  // 姿勢オフセット[pitch,roll,yaw]
 
 int mode = 0;
 
@@ -29,33 +25,17 @@ void OffsetMpu6886() {
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(2);
   int i = 0;
-  float accX_, accY_, accZ_;     // 加速度(offset)
-  float gyroX_, gyroY_, gyroZ_;  // 角速度(offset)
-  float pitch_, roll_, yaw_;     // 姿勢(offset)
+  float pitch_, roll_, yaw_;  // 姿勢(offset)
   while (i < 600) {
-    M5.Mpu6886.getAccelData(&accX_, &accY_, &accZ_);
-    M5.Mpu6886.getGyroData(&gyroX_, &gyroY_, &gyroZ_);
     M5.Mpu6886.getAhrsData(&pitch_, &roll_, &yaw_);
-    AccOffset[0] += accX_;
-    AccOffset[1] += accY_;
-    AccOffset[2] += accZ_;
-    GyroOffset[0] += gyroX_;
-    GyroOffset[1] += gyroY_;
-    GyroOffset[2] += gyroZ_;
     PRYOffset[0] += pitch;
     PRYOffset[1] += roll_;
     PRYOffset[2] += yaw_;
     i++;
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.printf("calibration:%d/600", i);
-    delay(100);
+    delay(50);
   }
-  AccOffset[0] /= i;
-  AccOffset[1] /= i;
-  AccOffset[2] /= i;
-  GyroOffset[0] /= i;
-  GyroOffset[1] /= i;
-  GyroOffset[2] /= i;
   PRYOffset[0] /= i;
   PRYOffset[1] /= i;
   PRYOffset[2] /= i;
@@ -76,10 +56,10 @@ void loop() {
 
   if (mode == 0) {
     M5.lcd.setCursor(0, 30);
-    M5.lcd.printf("Accel X:%7.2f\n      Y:%7.2f\n      Z:%7.2f\n", accX, accY, accZ);
-    M5.lcd.printf("Gyro  X:%7.2f\n      Y:%7.2f\n      Z:%7.2f\n", gyroX, gyroY, gyroZ);
+    // M5.lcd.printf("Accel X:%7.2f\n      Y:%7.2f\n      Z:%7.2f\n", accX, accY, accZ);
+    // M5.lcd.printf("Gyro  X:%7.2f\n      Y:%7.2f\n      Z:%7.2f\n", gyroX, gyroY, gyroZ);
     M5.lcd.printf("Ahrs  Pitch:%7.2f\n      Roll :%7.2f\n      Yaw  :%7.2f\n\n", pitch, roll, yaw);
-    M5.Lcd.printf("      Pitch:%7.2f\n      Roll :%7.2f", pitch2, roll2);
+    // M5.Lcd.printf("      Pitch:%7.2f\n      Roll :%7.2f", pitch2, roll2);
   } else if (mode == 1) {
     M5.lcd.fillScreen(BLACK);
     M5.lcd.drawCircle(160, 120, 90, WHITE);
@@ -94,39 +74,17 @@ void loop() {
 }
 
 void GetData() {
-  float acc[3], gyro[3];
-  float PRY1[3], PRY2[2];
+  float PRY1[3];
   int i = 0;
   while (i < 10) {
-    M5.Mpu6886.getAccelData(&accX, &accY, &accZ);
-    M5.Mpu6886.getGyroData(&gyroX, &gyroY, &gyroZ);
     M5.Mpu6886.getAhrsData(&pitch, &roll, &yaw);
-    roll2 = atan(accY / accZ) * RAD_TO_DEG;
-    pitch2 = atan(-accX / sqrtf(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-
-    acc[0] += (accX - AccOffset[0]);
-    acc[1] += (accY - AccOffset[1]);
-    acc[2] += (accZ - AccOffset[2]);
-    gyro[0] += (gyroX - GyroOffset[0]);
-    gyro[1] += (gyroY - GyroOffset[1]);
-    gyro[2] += (gyroZ - GyroOffset[2]);
     PRY1[0] += (pitch - PRYOffset[0]);
     PRY1[1] += (roll - PRYOffset[1]);
     PRY1[2] += (yaw - PRYOffset[2]);
-    PRY2[0] += (pitch2);
-    PRY2[1] += (roll2);
     i++;
     delay(10);
   }
-  accX = acc[0] / i;
-  accY = acc[1] / i;
-  accZ = acc[2] / i;
-  gyroX = gyro[0] / i;
-  gyroY = gyro[1] / i;
-  gyroZ = gyro[2] / i;
   pitch = PRY1[0] / i;
   roll = PRY1[1] / i;
   yaw = PRY1[2] / i;
-  pitch2 = PRY2[0] / i;
-  roll2 = PRY2[1] / i;
 }
